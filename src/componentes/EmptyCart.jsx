ffbfb02f7e3922cbb272a2c974ae58b42cf21698
manuelpolar01 <paragiliar1@hotmail.com>
2026-01-13 10:55:20 +0100

    proyecto final

--- src/componentes/EmptyCart.jsx
+++ src/componentes/EmptyCart.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const EmptyCart = () => {
   return (
     <div>
-      <h2> tu carrito esta vacio ☹</h2>
+      <h2> tu carrito esta vacio </h2>
       <h3> te invitamos a ver nuestros productos</h3>
       <Link className ='btn btn-dark' to='/'>volver home</Link>
     </div>
